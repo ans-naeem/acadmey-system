3eb797b0d8781f57650b2545d6e179a5d7bbc47a
ans-naeem <ans.naeem31@gmail.com>
2024-05-06 13:21:47 -0700

    cookies problem solved!!

--- TestMakerApril/bl/index.py
+++ TestMakerApril/bl/index.py
@@ -11,12 +11,14 @@ def index():
 @index_blueprint.route('/generateclasses/<queryname>')
 def generateclasses(queryname):
     dbhandler = databaseHandler()
+    if(queryname=="..."):
+        return"its a traiTOR"
     result=dbhandler.getter(queryname)
     #classes=[row[1]for row in result]
     classes_and_ids = [(row[0], row[1]) for row in result]
 
     serilized_Data=json.dumps(classes_and_ids)
-    response=make_response(render_template("generation.html",idsandname=classes_and_ids))
+    response=make_response(render_template("generateClasses.html",idsandname=classes_and_ids))
     response.set_cookie("class", '', expires=0)
     response.set_cookie("subject", '', expires=0)
     response.set_cookie("chapter", '', expires=0)
@@ -24,43 +26,44 @@ def generateclasses(queryname):
     #response.set_cookie('class',serilized_Data)
     return response
 
-@index_blueprint.route('/generatetable/<queryname>/<id>/<name>')
-def generatetable(queryname,id,name):
+@index_blueprint.route('/generatesubject/<queryname>/<id>/<name>')
+def generatesubject(queryname,id,name):
     dbhandler = databaseHandler()
     result=dbhandler.getterWithId(queryname,id)
     classes_and_ids = [(row[0], row[1]) for row in result]
     #passing query name to recognize in html pages
-    resp=make_response(render_template("dynamicgeneration.html",idsandname=classes_and_ids,query=queryname))
-    print(type(name))
-    # name=name.replace(" ","")
-    name=name.strip()
-
-    if("subject" in queryname.lower()):
-        resp.set_cookie("class",name)
-        print("there exist a subject in queryname")
-        print(request.cookies.get('class'))
-
-    if("chapter" in queryname.lower()):
-        resp.set_cookie("subject",name)
-        print("there exist a chapter in queryname")
-        print(request.cookies.get('subject'))
-
+    resp=make_response(render_template("generateSubjects.html",idsandname=classes_and_ids,query=queryname))
+    resp.set_cookie("class",name)
+    print(request.cookies.get('class'))
     return resp
 
+@index_blueprint.route('/generatechapter/<queryname>/<id>/<name>')
+def generatechapter(queryname,id,name):
+    dbhandler = databaseHandler()
+    result=dbhandler.getterWithId(queryname,id)
+    classes_and_ids = [(row[0], row[1]) for row in result]
+    #passing query name to recognize in html pages
+    resp=make_response(render_template("generateChapters.html",idsandname=classes_and_ids,query=queryname))
+    resp.set_cookie("subject",name)
+    print(request.cookies.get('subject'))
+    return resp
 
-
-@index_blueprint.route('/generatequestion/<queryname>/<id>/<type>')
-def generatequestion(queryname,id,type):
+@index_blueprint.route('/generatequestion/<queryname>',methods=['GET','POST'])
+def generatequestion(queryname):
+    selected_chapters=request.form.getlist('selected_chapters[]')
     dbhandler=databaseHandler()
-    result=dbhandler.getterWithId(queryname,id,type)
+    result=dbhandler.getterWithId(queryname,selected_chapters,"S")
     questions=[row[1] for row in result]
     clas=request.cookies.get('class')
     subject=request.cookies.get('subject')
-    print(clas)
-    print(request.cookies.get('subject'))
-    return render_template("generatequestions.html",questions=questions,clas=clas,subject=subject,type="Long" if type=='L' else "Short")
+    return render_template("generatequestions.html",questions=questions,clas=clas,subject=subject,type="Short")
 
 
+# @index_blueprint.route('/submitChapter/queryname',methods=['GET','POST'])
+# def submitChapter(queryname):
+#     dbhandler=databaseHandler()
+#     result=dbhandler.getterWithId(queryname,selected_chapters)
+#     return selected_chapters
 
 @index_blueprint.route('/submitquestion',methods=['GET','POST'])
 def submitquestion():
